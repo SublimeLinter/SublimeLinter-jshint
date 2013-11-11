d86437ea56fd6a45627b8953f354df6d3125e92b
Aparajita Fishman <aparajita@aparajita.com>
2013-11-11 00:13:37 -0500

    Initial commit

---
+++ jshint.py
@@ -0,0 +1,19 @@
+#
+# jshint.py
+# Linter for SublimeLinter3, a code checking framework for Sublime Text 3
+#
+# Written by Aparajita Fishman
+#
+# Project: https://github.com/SublimeLinter/SublimeLinter-contrib-jshint
+# License: MIT
+#
+
+from SublimeLinter.lint import Linter
+
+
+class JSHint(Linter):
+    language = 'javascript'
+    cmd = ('jshint', '--verbose')
+    regex = r'^.+?: line (?P<line>\d+), col (?P<col>\d+), (?P<error>.+) \((?P<type>[EW]\d+)\)$'
+    comment_re = r'\s*/[/*]'
+    tempfile_suffix = '.js'
