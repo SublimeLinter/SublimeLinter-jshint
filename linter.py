fedaefd891d353be48e894714e566537dc68e840
Martin Hansen <martin@martinhansen.no>
2014-01-14 11:18:34 +0100

    Added special highlightingof unused, non strict comparison and ++/--

--- linter.py
+++ linter.py
@@ -11,6 +11,7 @@
 """This module exports the JSHint plugin linter class."""
 
 from SublimeLinter.lint import Linter
+import re
 
 
 class JSHint(Linter):
@@ -22,7 +23,12 @@ class JSHint(Linter):
     regex = (
         r'^(?:(?P<fail>ERROR: .+)|'
         r'.+?: line (?P<line>\d+), col (?P<col>\d+), '
-        r'(?P<message>.+) \((?:(?P<error>E)|(?P<warning>W))\d+\))'
+        r'(?P<message>'
+        r'\'(?P<undef>.+)\'.+(?=.+W098)' #undefined warnings
+        r'|.+\'(?P<actual>.+)\'\.(?=.+W116)' #non strict operators
+        r'|.+\'(?P<unexpected>.+)\'\.(?=.+W016)' #unexpected use of ++ etc
+        r'|.+)' #match all messages
+        r' \((?:(?P<error>E)|(?P<warning>W))(?P<code>\d+)\))' #capture error, warning and code
     )
     selectors = {
         'html': 'source.js.embedded.html'
@@ -38,11 +44,35 @@ class JSHint(Linter):
 
         """
 
+        #restore word regex to default each iteration
+        self.word_re = None
+
         if match:
             fail = match.group('fail')
+            hasError = match.group('error')
+            hasWarning = match.group('warning')
+            message = match.group('message')
+            code = match.group('code')
+            line = int(match.group('line')) -1
+            col = int(match.group('col')) -1
+            near = None
+
 
             if fail:
                 # match, line, col, error, warning, message, near
                 return match, 0, 0, True, False, fail, None
+            #mark the undefined word
+            elif code == '098':
+                col = col - len(match.group('undef'))
+            #if we have a operator == or != manually change the column, near won't work here as we might have multiple ==/!= on a line
+            elif code == '116':
+                self.word_re = re.compile(match.group('actual'));
+                col = col - len(match.group('actual'))
+            #now jshint place the column in front, and as such we need to change our word matching regex, and keep the column info
+            elif code == '016':
+                self.word_re = re.compile('[+-]+');
+
+            return match, line, col, hasError, hasWarning, message, near
+
 
         return super().split_match(match)
