3945981c8cc551ac17026e1c19fbbca3a6a685eb
Sebastian Dawidziak <sebastian.dawidziak@profitroom.pl>
2015-02-24 10:50:24 +0100

    Fix newline

--- linter.py
+++ linter.py
@@ -16,7 +16,7 @@ from SublimeLinter.lint import persist
 
 
 class JSHint(Linter):
-
+    
     """Provides an interface to the jshint executable."""
     default_list = ["html", "laravel-blade", "javascript", "javascriptnext"]
     Linter.syntax_settings = persist.settings.get('jshint_syntax', default_list)
